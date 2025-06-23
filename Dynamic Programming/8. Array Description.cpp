6f3f46924f96715d0317df53f68665ea4e0c46b6
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2025-06-23 23:30:20 +0530

    tabulation lil different

--- Dynamic Programming/8. Array Description.cpp
+++ Dynamic Programming/8. Array Description.cpp
@@ -34,3 +34,49 @@ int main() {
 
   cout << f(0, 0) << "\n";
 }
+
+// tabulation 
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mod = 1e9 + 7;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for (int &x : a) cin >> x;
+
+    vector<vector<int>> dp(n, vector<int>(m + 2, 0)); // dp[i][val]
+
+    // Base case
+    if (a[n - 1] == 0) {
+        for (int val = 1; val <= m; val++) dp[n - 1][val] = 1;
+    } else {
+        dp[n - 1][a[n - 1]] = 1;
+    }
+
+    for (int i = n - 2; i >= 0; i--) {
+        for (int val = 1; val <= m; val++) {
+            if (a[i] != 0 && a[i] != val) continue; // only consider matching or 0
+            int ways = 0;
+            for (int d = -1; d <= 1; d++) {
+                int next_val = val + d;
+                if (next_val >= 1 && next_val <= m) {
+                    ways = (ways + dp[i + 1][next_val]) % mod;
+                }
+            }
+            dp[i][val] = ways;
+        }
+    }
+
+    int result = 0;
+    if (a[0] == 0) {
+        for (int val = 1; val <= m; val++) {
+            result = (result + dp[0][val]) % mod;
+        }
+    } else {
+        result = dp[0][a[0]];
+    }
+    cout << result << "\n";
+    return 0;
+}
